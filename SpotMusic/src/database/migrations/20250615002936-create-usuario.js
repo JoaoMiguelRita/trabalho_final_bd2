af4fd5be58738119e2a469a596b88d74d796fb9e
JoaoMiguel <joaomiguelrita01@gmail.com>
2025-06-14 21:41:26 -0300

    Primeira Migration realizada

---
+++ SpotMusic/src/database/migrations/20250615002936-create-usuario.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('usuarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id_usuario: {
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      senha: {
+        type: Sequelize.STRING
+      },
+      dt_nascimento: {
+        type: Sequelize.DATE
+      },
+      tipo_conta: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('usuarios');
+  }
+};
---
+++ SpotMusic/src/models/usuario.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class usuario extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  usuario.init({
+    id_usuario: DataTypes.INTEGER,
+    nome: DataTypes.STRING,
+    email: DataTypes.STRING,
+    senha: DataTypes.STRING,
+    dt_nascimento: DataTypes.DATE,
+    tipo_conta: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'usuario',
+  });
+  return usuario;
+};
